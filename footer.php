c76b7d4cfe2a26a7a09747078102d5cac4a05cfb
giangson209 <ledinhson209@gmail.com>
2023-01-11 03:09:35 +0700

    update 1

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer></footer>
+<script type="text/javascript" src="js/lib/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/lib/slick.min.js"></script> 
+<script type="text/javascript" src="js/private.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,162 @@
+<?php include 'header.php';?>
+<main>
+    <section class="box-bannner">
+        <div class="avarta-banner"><img src="images/banner.jpg" class="img-fluid w-100" alt=""></div>
+        <div class="caption-banner">
+            <div class="container">
+                <div class="info-caption text-center">
+                    <h1>We’re optimizing the way <br>you buy jewellery.</h1>
+                    <div class="link-banner">
+                        <a href="">Get your unique piece of jewellery <img src="images/arrow-right.png" class="img-fluid" alt=""></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="btn-scroll text-center"><a href=""><img src="images/btn-scoll.png" class="img-fluid" alt=""></a></div>
+    </section>
+    <section class="box-about">
+        <div class="content-about">
+            <div class="left">
+                <div class="txt-about">
+                    <div class="title">
+                        <h2><span>Get to know <br>us better.</span></h2>
+                    </div>
+                    <div class="desc">
+                        <p>Whether your style is super simple, or a little funky, we'll help you create that one, absolutely perfect piece of jewellery, and leave you with a very special story to tell.</p>
+                        <p>All our exclusive and unique jewellery is 100% designed and manufactured in <br>Antwerp. </p>
+                    </div>
+                    <div class="link-detail"><a href="">More about us <img src="images/arrow-right.png" class="img-fluid" alt=""></a></div>
+                </div>
+            </div>
+            <div class="right">
+                <div class="avarta"><img src="images/about.jpg" class="img-fluid w-100" alt=""></div>
+            </div>
+        </div>
+    </section>
+    <section class="box-budget">
+        <div class="container">
+            <div class="title text-center">
+                <h2>The perfect fit for any budget</h2>
+            </div>
+            <div class="content-budget text-center">
+                <p><img src="images/butget.png" class="" alt=""></p>
+                <p>We don’t do pre-made rings with price tags on them. Our goal is to create an amazing piece of jewellery that fits your budget. Something made just for the love of your life, for the price of something off the shelf. </p>
+                <p>Please keep in mind that we always work with real, natural diamonds and 10, 14 or 18 Carat gold. Of course this has a certain price range, but we’ll make sure we can help you out. </p>
+                <p>Once you are satisfied with the design within your budget, we can proceed to production . Depending on the project, waiting periods are usually from 3 to 5 weeks max. A wonderful and exciting period to look forward to what you helped design. Once your unique piece of jewellery is ready, we welcome you to our main office to discover your jewel for the first time. </p>
+            </div>
+            <div class="link-detail text-center"><a href="">CHECK WHAT GOES INTO PRICING <img src="images/arrow-right.png" class="img-fluid" alt=""></a></div>
+        </div>
+    </section>
+    <section class="box-discover">
+        <div class="container">
+            <div class="title text-center">
+                <h2>Discover our hidden gems</h2>
+            </div>
+            <div class="list-gems">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="item-gem"><a href=""><img src="images/gem-1.png" class="img-fluid w-100" alt=""></a></div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="item-gem"><a href=""><img src="images/gem-2.png" class="img-fluid w-100" alt=""></a></div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="item-gem"><a href=""><img src="images/gem-3.png" class="img-fluid w-100" alt=""></a></div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="item-gem"><a href=""><img src="images/gem-4.png" class="img-fluid w-100" alt=""></a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="link-detail text-center"><a href=""><span>SHOW MORE hiddacen gems</span> <img src="images/arrow-down.png" class="img-fluid" alt=""></a></div>
+        </div>
+    </section>
+    <section class="box-payment">
+        <div class="content-payment">
+            <div class="left">
+                <div class="avarta"><img src="images/about.jpg" class="img-fluid w-100" alt=""></div>
+            </div>
+            <div class="right">
+                <div class="txt-payment">
+                    <div class="title">
+                        <h2><span>Some practical info regarding payments</span></h2>
+                    </div>
+                    <div class="desc">
+                        <p>Diamonds are about trust. Before starting the project we agree on the budget and the delivery date. After that, 50% of the total amount is received before starting the project . </p>
+                        <p>When your project is finished and you are 100% satisfied , the remaining amount will be charged. This way of working allows you to pay in two</p>
+                        <p>installments and provides us with a guarantee that you will actually collect the jewellery made especially for you.</p>
+                    </div>
+                    <div class="link-detail"><a href="">Get your unique piece of jewellery <img src="images/arrow-right.png" class="img-fluid" alt=""></a></div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="box-form">
+        <div class="container">
+            <div class="title text-center">
+                <h2><span>The perfect fit for any budget</span></h2>
+            </div>
+            <div class="desc-form text-center">
+                <p>When it comes to rings, jewellery & diamonds, everyone wishes they “knew somebody.”</p>
+                <p>Well, we’re that somebody. We’re the experts so you don’t have to be one.</p>
+            </div>
+            <div class="touch-form text-center"><span>Let’s get in touch!</span></div>
+            <div class="frm-content">
+                <div class="item">
+                    <select name="" id="">
+                        <option value="">SELECT YOUR BUDGET</option>
+                        <option value="">5</option>
+                        <option value="">10</option>
+                        <option value="">15</option>
+                    </select>
+                </div>
+                <div class="item">
+                    <select name="" id="">
+                        <option value="">Choose your color</option>
+                        <option value="">White</option>
+                        <option value="">Green</option>
+                        <option value="">Pink</option>
+                    </select>
+                </div>
+                <div class="item">
+                    <select name="" id="">
+                        <option value="">SELECT A SIZE</option>
+                        <option value="">12</option>
+                        <option value="">14</option>
+                        <option value="">16</option>
+                    </select>
+                </div>
+                <div class="item">
+                    <select name="" id="">
+                        <option value="">DATE OF DELIVERY</option>
+                        <option value="">12</option>
+                        <option value="">14</option>
+                        <option value="">16</option>
+                    </select>
+                </div>
+                <div class="item">
+                    <select name="" id="">
+                        <option value="">Where Will We Be Shipping Your Project?</option>
+                        <option value="">12</option>
+                        <option value="">14</option>
+                        <option value="">16</option>
+                    </select>
+                </div>
+                <div class="item">
+                    <div class="item-file">
+                        <p>Share some inspiration with us</p>
+                        <input type="file" class="inp_file" id="up_file"><label for="up_file" class="mb-0">UPLOAD PHOTO</label>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="item-btn">
+                        <input type="submit" class="btn_field" value="SENT">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+<?php include 'footer.php';?>
+
+
